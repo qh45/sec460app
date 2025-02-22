a6e8f9c87f87c26c7f39da880efdc04f8f681531
qh45 <quinnhillberg@hotmail.com>
2025-02-22 22:42:12 +0000

    Added fortune teller app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,114 +1,36 @@
 import streamlit as st
-import pygame
-import time
 import random
 
-pygame.init()
+# List of random messages
+messages = [
+    "Good news will come to you by mail.",
+    "A fresh start will put you on your way.",
+    "A friend asks only for your time not your money.",
+    "A golden opportunity is heading your way.",
+    "A smile is your personal welcome mat.",
+    "All your hard work will soon pay off.",
+    "An important person will offer you support.",
+    "Believe in yourself and others will too.",
+    "Change is happening in your life, so go with the flow!",
+    "Distance yourself from the negativity of others.",
+    "Embrace change and make it work in your favor.",
+    "Expect great things and great things will come to you.",
+    "Great things are coming your way.",
+    "Happiness will soon knock on your door.",
+    "Keep your goals in sight and go for it.",
+    "Look forward to a new adventure.",
+    "Seize every opportunity that comes your way.",
+    "Success is on your horizon.",
+    "You will make a new friend soon.",
+    "Your future is bright and full of possibilities."
+]
 
-# Set the width and height of the screen (width, height).
-screen_width = 600
-screen_height = 400
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Snake Game')
-
-# Colors
-black = (0, 0, 0)
-white = (255, 255, 255)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
-
-snake_block = 10
-snake_speed = 15
-
-clock = pygame.time.Clock()
-
-font_style = pygame.font.SysFont(None, 35)
-
-def our_snake(snake_block, snake_list):
-    for x in snake_list:
-        pygame.draw.rect(screen, green, [x[0], x[1], snake_block, snake_block])
-
-def message(msg, color):
-    mesg = font_style.render(msg, True, color)
-    screen.blit(mesg, [screen_width / 6, screen_height / 3])
-
-def gameLoop():
-    game_over = False
-    game_close = False
-
-    x1 = screen_width / 2
-    y1 = screen_height / 2
-
-    x1_change = 0
-    y1_change = 0
-
-    snake_List = []
-    Length_of_snake = 1
-
-    foodx = round(random.randrange(0, screen_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, screen_height - snake_block) / 10.0) * 10.0
-
-    while not game_over:
-
-        while game_close == True:
-            screen.fill(blue)
-            message("You Lost! Press Q-Quit or C-Play Again", red)
-            pygame.display.update()
-
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
-                    if event.key == pygame.K_c:
-                        gameLoop()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_RIGHT:
-                    x1_change = snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_UP:
-                    y1_change = -snake_block
-                    x1_change = 0
-                elif event.key == pygame.K_DOWN:
-                    y1_change = snake_block
-                    x1_change = 0
-
-        if x1 >= screen_width or x1 < 0 or y1 >= screen_height or y1 < 0:
-            game_close = True
-        x1 += x1_change
-        y1 += y1_change
-        screen.fill(black)
-        pygame.draw.rect(screen, red, [foodx, foody, snake_block, snake_block])
-        snake_Head = []
-        snake_Head.append(x1)
-        snake_Head.append(y1)
-        snake_List.append(snake_Head)
-        if len(snake_List) > Length_of_snake:
-            del snake_List[0]
-
-        for x in snake_List[:-1]:
-            if x == snake_Head:
-                game_close = True
-
-        our_snake(snake_block, snake_List)
-        pygame.display.update()
-
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, screen_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, screen_height - snake_block) / 10.0) * 10.0
-            Length_of_snake += 1
-
-        clock.tick(snake_speed)
-
-    pygame.quit()
-    quit()
-
-gameLoop()
+def main():
+    st.title("Fortune Teller")
+    
+    if st.button("Click to see your fortune"):
+        message = random.choice(messages)
+        st.write(message)
+        
+if __name__ == "__main__":
+    main()
